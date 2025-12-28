254ec32ef9cddc060ca7f6d4a35ee2e77198c014
Surya <suryatk2007@gmail.com>
2025-12-28 15:22:43 +0530

    feat: Introduce new landing page, add Tailwind CSS integration, and include logo asset.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import TestPage from './pages/TestPage';
+import LandingPage from './pages/LandingPage';
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<TestPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </>
   )
